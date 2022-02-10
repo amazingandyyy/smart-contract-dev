6f51bc23c5c13037c68f257437e0fa83d71ebfc2
Andy Chen <amazingandyyy@gmail.com>
2022-02-10 21:13:28 +0000

    feat: init template

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+const pretterrc = require('.pretterrc');
+
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:node/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  overrides: [
+    {
+      files: ['hardhat.config.js'],
+      globals: { task: true },
+    },
+  ],
+  rules: {
+    'prettier/prettier': ['error', pretterrc],
+  },
+};
